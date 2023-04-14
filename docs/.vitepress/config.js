b7a280aa3deebda1b6f8986a0517ec61ca020c04
wzkMaster <727498469@qq.com>
2023-04-15 01:49:55 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vitepress";
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "信管课程资料清单",
+  description: "信管专业学习资料推荐",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [{ text: "首页", link: "/" }],
+
+    sidebar: [
+      {
+        text: "前言",
+        link: "/",
+      },
+      {
+        text: "数学",
+        link: "/math",
+      },
+      {
+        text: "编程",
+        link: "/programming",
+      },
+      {
+        text: "其他课程",
+        link: "/other",
+      },
+      {
+        text: "工具推荐",
+        link: "/tools",
+      },
+      {
+        text: "经验分享",
+        link: "/experience",
+      },
+    ],
+
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
